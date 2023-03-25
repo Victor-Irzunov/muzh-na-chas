0b69503105ed5537c4fd0d71716fa9ad1dd66af7
Виктор <viktor@MacBook-Air-Viktor.local>
2023-03-25 15:17:25 +0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#050816",
+        secondary: "#aaa6c3",
+        tertiary: "#151030",
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xyy: "318px",
+        xy: "373px",
+        xx: "415px",
+        xs: "480px",
+        xm: "575px",
+        ss: "620px",
+        sm: "768px",
+        sd: "991px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
